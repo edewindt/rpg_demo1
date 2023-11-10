06ecc949f02cbe043a54eee60af0ee3ccd06d9cb
Elias De Windt <edewindt27@outlook.com>
2023-11-10 00:16:00 -0800

    different sprites attached to keys

--- main.go
+++ main.go
@@ -3,34 +3,40 @@ package main
 import (
 	"image"
 	"log"
+	"strings"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type Game struct {
-	spriteSheet  *ebiten.Image
 	frameWidth   int
 	frameHeight  int
 	frameCount   int
 	currentFrame int
 	tickCount    int // Counter to track the number of updates
 	x, y         float64
+	spriteSheets map[string]*ebiten.Image // Map of sprite sheets for each direction
+	direction    string
 }
 
 func (g *Game) Update() error {
 
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		g.x -= 2 // Move left
+		g.direction = "left"
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
 		g.x += 2 // Move right
+		g.direction = "right"
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
 		g.y -= 2 // Move up
+		g.direction = "up"
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyDown) {
 		g.y += 2 // Move down
+		g.direction = "down"
 	}
 	// Increment the tick count
 	g.tickCount++
@@ -44,15 +50,21 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	currentSpriteSheet := g.spriteSheets[g.direction]
 	// Determine the x, y location of the current frame on the sprite sheet
-	sx := (g.currentFrame % (g.spriteSheet.Bounds().Dx() / g.frameWidth)) * g.frameWidth
-	sy := (g.currentFrame / (g.spriteSheet.Bounds().Dx() / g.frameWidth)) * g.frameHeight
+	sx := (g.currentFrame % (currentSpriteSheet.Bounds().Dx() / g.frameWidth)) * g.frameWidth
+	sy := (g.currentFrame / (currentSpriteSheet.Bounds().Dx() / g.frameWidth)) * g.frameHeight
 
 	// Create a sub-image that represents the current frame
-	frame := g.spriteSheet.SubImage(image.Rect(sx, sy, sx+g.frameWidth, sy+g.frameHeight)).(*ebiten.Image)
+	frame := currentSpriteSheet.SubImage(image.Rect(sx, sy, sx+g.frameWidth, sy+g.frameHeight)).(*ebiten.Image)
 
 	// Draw the sub-image on the screen
 	opts := &ebiten.DrawImageOptions{}
+	// If the direction is left, flip the image on the vertical axis
+	if g.direction == "left" {
+		opts.GeoM.Scale(-1, 1)                        // Flip horizontally
+		opts.GeoM.Translate(float64(g.frameWidth), 0) // Adjust the position after flipping
+	}
 	opts.GeoM.Translate(g.x, g.y)
 	screen.DrawImage(frame, opts)
 }
@@ -61,19 +73,46 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 	return 320, 240
 }
 
+func loadSpriteSheets() map[string]*ebiten.Image {
+	// Create a map to hold the sprite sheets
+	spriteSheets := make(map[string]*ebiten.Image)
+
+	// List of directions
+	directions := []string{"up", "down", "right", "left"}
+
+	// Loop over the directions and load the corresponding sprite sheet
+	for _, direction := range directions {
+		// Construct the file path for the sprite sheet
+		// This assumes you have files named like "playerUp.png", "playerDown.png", etc.
+		if direction == "left" {
+			spriteSheets["left"] = spriteSheets["right"]
+			break
+		}
+		path := "assets/player" + strings.Title(direction) + ".png"
+
+		// Load the image
+		img, _, err := ebitenutil.NewImageFromFile(path)
+		if err != nil {
+			log.Fatalf("failed to load '%s' sprite sheet: %v", direction, err)
+		}
+
+		// Store the loaded image in the map
+		spriteSheets[direction] = img
+	}
+
+	return spriteSheets
+}
 func main() {
 	// Load the sprite sheet
-	spriteSheet, _, err := ebitenutil.NewImageFromFile("assets/playerDown.png")
-	if err != nil {
-		log.Fatal(err)
-	}
+	spriteSheets := loadSpriteSheets()
 
 	// Create an instance of the Game struct
 	game := &Game{
-		spriteSheet: spriteSheet,
-		frameWidth:  192 / 4, // The width of a single frame
-		frameHeight: 68,      // The height of a single frame
-		frameCount:  4,       // The total number of frames in the sprite sheet
+		spriteSheets: spriteSheets,
+		frameWidth:   192 / 4, // The width of a single frame
+		frameHeight:  68,      // The height of a single frame
+		frameCount:   4,       // The total number of frames in the sprite sheet
+		direction:    "down",  // Default direction
 	}
 
 	// Configuration settings
