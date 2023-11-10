8048acdb8e304f5c4291edb07c72e8ed8a61e57d
Elias De Windt <edewindt27@outlook.com>
2023-11-09 23:31:01 -0800

    image drawn to screen

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	image *ebiten.Image
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	opts := &ebiten.DrawImageOptions{}
+	screen.DrawImage(g.image, opts)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -22,9 +25,19 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	img, _, err := ebitenutil.NewImageFromFile("assets/playerDown.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an instance of your game
+	game := &Game{
+		image: img,
+	}
+
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowTitle("My Game")
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
